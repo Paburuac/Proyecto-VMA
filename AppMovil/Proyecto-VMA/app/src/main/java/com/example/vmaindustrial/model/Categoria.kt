f82ea18eb6f227cda91d993ffbdd1c03c321c0ab
Diego-Aros <dieg.aros@duocuc.cl>
2026-06-24 13:57:49 -0400

    Revert "Se añadio appMovil"
    This reverts commit 5e3f9f47d498c8ed1a9c0970e0f31863b946e604.

--- AppMovil/Proyecto-VMA/app/src/main/java/com/example/vmaindustrial/model/Categoria.kt
+++
@@ -1,9 +0,0 @@
-package com.example.vmaindustrial.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Categoria(
-    val id_categoria: Int,
-    val nombre_categoria: String
-)
