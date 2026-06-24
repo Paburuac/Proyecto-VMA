b82e6d9129a66a614c2596f4dac5f3e0b83640c7
Diego-Aros <dieg.aros@duocuc.cl>
2026-06-24 14:01:19 -0400

    Revert "Merge branch 'main' of https://github.com/Paburuac/Proyecto-VMA"
    This reverts commit 169d6e37545a6fcae1e53c57d47fcf3ae3a29762, reversing
    changes made to f82ea18eb6f227cda91d993ffbdd1c03c321c0ab.

--- Producto/AppMovil/Proyecto-VMA/app/src/main/java/com/example/vmaindustrial/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.example.vmaindustrial.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val white = Color(0xFFFFFFFF)
-val blue = Color(0xFF004C7F)
-val green = Color(0xFF9ACD00)
-
-val DarkGray = Color(0xFF282828)
